ac36e7ff64c28dd8ca414acca3d21cc703c3afa0
= <=>
2022-02-05 12:29:19 +0300

    path and server fixes

--- server.js
+++ server.js
@@ -5,4 +5,8 @@ app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html')
 })
 
+app.get('/register', (req, res) => {
+	res.sendFile(__dirname + '/register.html')
+})
+
 app.listen(process.env.port || 3000)
